37647856d6fa2e7804a8dd4f454dbf55d6b7643d
YordanStoilov <iordan27@gmail.com>
2024-12-06 13:57:20 +0200

    solved task 2

--- 01.ClassesAndObjects/rust.cpp
+++ 01.ClassesAndObjects/rust.cpp
@@ -18,27 +18,84 @@ public:
             stream << '\n';
         }
     }
+    void rustCells(){
+        putTemporaryRust();
+        putRust();
+    }
+    void putTemporaryRust(){
+        for (size_t row = 0; row < SIZE; ++row){
+            for (size_t col = 0; col < SIZE; ++col){
+                if (mtx[row][col] == RUST){
+                    // Up
+                    if (row > 0){
+                        if (mtx[row - 1][col] == HEALTHY){
+                            mtx[row - 1][col] = TEMP_RUST;
+                        }
+                    }
+                    // Down
+                    if (row < SIZE - 1)
+                    {
+                        if (mtx[row + 1][col] == HEALTHY){
+                            mtx[row + 1][col] = TEMP_RUST;
+                        }
+                    }
+                    // Left
+                    if (col > 0){
+                        if (mtx[row][col - 1] == HEALTHY){
+                            mtx[row][col - 1] = TEMP_RUST;
+                        }
+                    }
+                    // Right
+                    if (col < SIZE - 1){
+                        if (mtx[row][col + 1] == HEALTHY){
+                            mtx[row][col + 1] = TEMP_RUST;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    void putRust(){
+        for (size_t row = 0; row < SIZE; ++row){
+            for (size_t col = 0; col < SIZE; ++col){
+                if (mtx[row][col] == TEMP_RUST){
+                    mtx[row][col] = RUST;
+                }
+            }
+        }
+    }
+    static const size_t SIZE = 10;
 
 private:
-    static const size_t SIZE = 10;
-    static const char INFECTION = '!';
-    static const char HEALTHY_CELL = '.';
+    static const char RUST = '!';
+    static const char HEALTHY = '.';
+    static const char TEMP_RUST = '?';
     static const char RESISTANT = '#';
     char mtx[SIZE][SIZE];
 };
+
 class RustProcessor{
 public:
     RustProcessor(std::istream& stream) : plate(stream), iterations(0){
         stream >> iterations;
     }
+    void rust(){
+        size_t iterations = this->iterations;
+        for (;iterations; iterations--){
+            plate.rustCells();
+        }
+    }
+    void go(std::ostream& stream){
+        rust();
+        plate.print(stream);
+    }
 
 private:
     size_t iterations;
     Plate plate;
-
 };
 
 int main(){
     RustProcessor rp(std::cin);
-
+    rp.go(std::cout);
 }
