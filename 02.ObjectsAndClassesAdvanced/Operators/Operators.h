0873fb7cf583b8a02385a25a83a3c8aa096ac7c3
YordanStoilov <iordan27@gmail.com>
2024-12-08 21:42:03 +0200

    solved task 3 from exercise

--- 02.ObjectsAndClassesAdvanced/Operators/Operators.h
+++ 02.ObjectsAndClassesAdvanced/Operators/Operators.h
@@ -1,11 +1,22 @@
 #include <vector>
 #include <string>
+#include <sstream>
 
 using std::vector;
 using std::string;
 
-vector<string> operator<<(vector<string>& vector, string& str)
+vector<string>& operator<<(vector<string>& vector, const string& str)
 {
     vector.push_back(str);
     return vector;
+}
+string operator+(string& str, int number){
+    std::ostringstream oss;
+    oss << str << number;
+    return oss.str();
+}
+std::ostream& operator<<(std::ostream& out, const vector<string> strings){
+    for (auto& str : strings)
+        out << str << std::endl;
+    return out;
 }
