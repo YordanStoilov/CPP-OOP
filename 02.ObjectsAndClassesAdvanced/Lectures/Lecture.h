202a38d17c3cd770c00f4b394c3395402d57327f
YordanStoilov <iordan27@gmail.com>
2024-12-09 15:13:39 +0200

    solved task 5 from exercise

--- 02.ObjectsAndClassesAdvanced/Lectures/Lecture.h
+++ 02.ObjectsAndClassesAdvanced/Lectures/Lecture.h
@@ -1,21 +1,51 @@
-#ifndef LECTURE.H
-#define LECTURE.H
+#ifndef LECTURE_H
+#define LECTURE_H 
 #include <set>
+#include <map>
+#include <vector>
+#include "Resource.h"
 
 namespace SoftUni{
     class Lecture{
     public:
-        std::__1::set<Resource>::iterator begin(){
+        std::set<Resource>::iterator begin(){
             return resources.begin();
         }
-        std::__1::set<Resource>::iterator end(){
+        std::set<Resource>::iterator end(){
             return resources.end();
         }
-        // There can be 2 resource objects with the same ID and different links.
-        // 
-    public:
-        static std::set<Resource> resources;
-    };
-}
+        int operator[](ResourceType rt){
+            return count[rt];
+        }
+    private:
+        std::set<Resource> resources;
+        std::map<SoftUni::ResourceType, int> count;
 
+        friend Lecture& operator<<(Lecture& l, Resource& r);
+        friend std::vector<ResourceType>& operator<<(std::vector<ResourceType>& vec, Lecture& l);
+    };
+
+    Lecture& operator<<(Lecture& l, Resource& r){
+        ResourceType rt = r.getType();
+        auto itFound = l.resources.find(r);
+
+        if (itFound != l.resources.end()){
+                l.resources.erase(itFound);
+                l.count[rt]--;
+                if (l.count[rt] == 0){
+                    l.count.erase(rt);
+                }
+            }
+            l.resources.insert(r);
+            l.count[rt]++;
+            return l;
+        }
+    std::vector<ResourceType>& operator<<(std::vector<ResourceType>& vec, Lecture& l){
+        // Use count map
+        for (auto [key, value] : l.count){
+            vec.push_back(key);
+        }
+        return vec;
+    }
+}
 #endif
