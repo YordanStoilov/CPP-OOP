271ac58d054da7e8599ffc2886fded2112ffd1f3
YordanStoilov <iordan27@gmail.com>
2024-12-17 16:12:38 +0200

    SmartArray works as intended

--- 05.RuleOf3-5-0/SmartArray/main.cpp
+++ 05.RuleOf3-5-0/SmartArray/main.cpp
@@ -1,5 +1,18 @@
 #include "SmartArray.h"
 
 int main(){
+    SmartArray<int> nums;
 
+    nums.push_back(1);
+    nums.push_back(2);
+    nums.push_back(3);
+    nums.push_back(4);
+    nums.push_back(5);
+    nums.push_back(6);
+    nums.push_back(7);
+
+    nums[nums.size() - 2] = 100;
+    nums.pop_back();
+
+    nums.print();
 }
