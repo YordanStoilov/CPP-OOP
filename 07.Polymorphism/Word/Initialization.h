1b2736813282dcb4f450dfeac9615307f5f30a1c
YordanStoilov <iordan27@gmail.com>
2025-01-02 15:53:12 +0200

    solved task 4

--- 07.Polymorphism/Word/Initialization.h
+++ 07.Polymorphism/Word/Initialization.h
@@ -5,50 +5,50 @@
 
 class AdvancedCI : public CommandInterface {
     public:
-        AdvancedCI(std::string& text) : CommandInterface(text) 
+        AdvancedCI(std::string& text) : CommandInterface(text), 
+            cutTransform(std::shared_ptr<CutTransform>(new CutTransform))
         {}
         virtual ~AdvancedCI() = default;
 
         class CutTransform : public TextTransform {
-            AdvancedCI* parent;
+            std::string content;
 
             public:
-                explicit CutTransform(AdvancedCI* parent) : parent(parent)
-                {}
                 virtual ~CutTransform() = default;
 
                 virtual void invokeOn(std::string& text, int startIndex, int endIndex) override {
                     if (startIndex == endIndex) {
-                        parent->memory.clear();
+                        content.clear();
                         return;
                     }
-                    parent->memory = text.substr(startIndex, endIndex - startIndex);
+                    content = text.substr(startIndex, endIndex - startIndex);
                     text.erase(startIndex, endIndex - startIndex);
                 }
+                std::string getContent() const { return content; }
         };
         class PasteTransform : public TextTransform {
-            AdvancedCI* parent;
-
+            protected:
+                std::shared_ptr<CutTransform> cutTransform;
             public:
-                explicit PasteTransform(AdvancedCI* parent) : parent(parent)
+                PasteTransform(std::shared_ptr<CutTransform> cutTransform) 
+                    : cutTransform(cutTransform)
                 {}
                 virtual ~PasteTransform() = default;
 
                 virtual void invokeOn(std::string& text, int startIndex, int endIndex) override {
-                    if (startIndex == endIndex) {
-                        text.insert(startIndex, parent->memory);
-                    }
-                    text.replace(text.begin() + startIndex, text.begin() + endIndex, parent->memory);
+                    text.erase(startIndex, endIndex - startIndex);
+                    text.insert(startIndex, cutTransform->getContent());
                 }
         };
     protected:
         std::string memory;
-        friend TextTransform;
+        std::shared_ptr<CutTransform> cutTransform;
+
         virtual std::vector<Command> initCommands() override {
             std::vector<Command> commands = CommandInterface::initCommands();
 
-            commands.push_back(Command("cut", std::make_shared<CutTransform>(this)));
-            commands.push_back(Command("paste", std::make_shared<PasteTransform>(this)));
+            commands.push_back(Command("cut", cutTransform));
+            commands.push_back(Command("paste", std::make_shared<PasteTransform>(cutTransform)));
 
             return commands;
         }
