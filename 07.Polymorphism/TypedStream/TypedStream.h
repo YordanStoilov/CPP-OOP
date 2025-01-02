4fd55bd3d540bf92b6bf3c8852bcaa444c8f2a2a
YordanStoilov <iordan27@gmail.com>
2025-01-02 12:07:06 +0200

    fixed bug and solved task 3

--- 07.Polymorphism/TypedStream/TypedStream.h
+++ 07.Polymorphism/TypedStream/TypedStream.h
@@ -1,25 +1,35 @@
 #ifndef TYPED_STREAM_H
 #define TYPED_STREAM_H
+#include <string>
+#include <vector>
 #include <sstream>
 
-template <typename T>
-class TypedStream {
-public:
-    TypedStream(const std::string input) : input(input) {
-    }
-    virtual TypedStream<std::string>& operator>>(std::string& s) {
-    }
-    virtual std::string& operator>>(std::ostream& stream, std::string& s) {
-    s = stream.str();
-    return s;
-}
 
-    std::string readToEnd(void) {
-        return stream.str();
+template <class T>
+class TypedStream {
+
+public:
+    TypedStream(const std::string& input) : stream(std::istringstream(input))
+    {}
+    virtual TypedStream<T>& operator>>(T& i) {
+        return *this;
     }
+    std::vector<T> readToEnd() {
+        std::vector<T> result;
+
+        T curr;
+        while (true) {
+            *this >> curr;
+            if ((bool)stream == false) {
+                break;
+            }
+            result.push_back(curr);
+        }
+        return result;
+    }
+    virtual ~TypedStream() = default;
 protected:
-    std::string input;
-    std::ostringstream stream;
-}; 
+    std::istringstream stream;
+};
 
 #endif
